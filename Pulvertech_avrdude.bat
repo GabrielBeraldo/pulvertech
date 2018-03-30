71f00e96b2e6cac240a2804e8c812afbfefdf394
GabrielBeraldo <gabrielgbst@hotmail.com>
2018-03-30 12:59:06 -0300

    add programmer

---
+++ Pulvertech_avrdude.bat
@@ -0,0 +1,5 @@
+
+color a
+C:\Users\gabri\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino9/bin/avrdude -CC:\Users\gabri\AppData\Local\Arduino15\packages\arduino\tools\avrdude\6.3.0-arduino9/etc/avrdude.conf -v -patmega328p -cusbasp -Pusb -Uflash:w:D:\pulvertech\pulvertech_V1\Pulvertech_V1_raw\.pioenvs\uno/firmware.hex:i
+
+pause
